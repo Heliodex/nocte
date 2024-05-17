2be41eb203fc0a83fdd07bfd4f102550e2d2aa98
Lewin Kelly <heli@odex.cf>
2024-05-17 23:56:45 +0100

    Call external processes from Luau

--- ext/main.go
+++ ext/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"crypto/sha256"
-	"encoding/csv"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"math/big"
 	"os"
 )
@@ -115,27 +114,6 @@ func mul(p1 *Point, n1 *big.Int) *Point {
 	return r
 }
 
-func bytesFromInt(i *big.Int) []byte {
-	b := i.Bytes()
-	// pad out to 32 bytes
-	if len(b) < 32 {
-		b = append(bytes.Repeat([]byte{0}, 32-len(b)), b...)
-	}
-	return b
-}
-
-func bytesFromPoint(p Point) []byte {
-	return bytesFromInt(x(&p))
-}
-
-func xorBytes(b1, b2 []byte) []byte {
-	b := make([]byte, len(b1))
-	for i := range b1 {
-		b[i] = b1[i] ^ b2[i]
-	}
-	return b
-}
-
 func liftX(x *big.Int) *Point {
 	if x.Cmp(p) >= 0 {
 		return nil
@@ -175,72 +153,6 @@ func hasEvenY(p *Point) bool {
 	return new(big.Int).Mod(y(p), big.NewInt(2)).Cmp(big.NewInt(0)) == 0
 }
 
-func pubkeyGen(seckey []byte) []byte {
-	d0 := intFromBytes(seckey)
-	// if d0 < 1 or d0 > n - 1
-	if d0.Cmp(big.NewInt(1)) < 0 || d0.Cmp(new(big.Int).Sub(n, big.NewInt(1))) > 0 {
-		panic("The secret key must be an integer in the range 1..n-1.")
-	}
-	P := mul(&G, d0)
-	assertNotInfinity(P)
-	return bytesFromPoint(*P)
-}
-
-func schnorrSign(message, seckey, auxRand []byte) []byte {
-	d0 := intFromBytes(seckey)
-	// if d0 < 1 or d0 > n - 1
-	if d0.Cmp(big.NewInt(1)) < 0 || d0.Cmp(new(big.Int).Sub(n, big.NewInt(1))) > 0 {
-		panic("The secret key must be an integer in the range 1..n-1.")
-	} else if len(auxRand) != 32 {
-		panic("auxRand must be 32 bytes")
-	}
-	P := mul(&G, d0)
-	assertNotInfinity(P)
-	var d *big.Int
-	if hasEvenY(P) {
-		d = d0
-	} else {
-		d = new(big.Int).Sub(n, d0)
-	}
-	t := xorBytes(bytesFromInt(d), taggedHash("BIP0340/aux", auxRand))
-	// hashTag = t + bytesFromPoint(P) + message
-	hashTag := append(t, bytesFromPoint(*P)...)
-	hashTag = append(hashTag, message...)
-	k0 := new(big.Int).Mod(intFromBytes(taggedHash("BIP0340/nonce", hashTag)), n)
-	if k0.Cmp(big.NewInt(0)) == 0 {
-		panic("Failure. This happens only with negligible probability.")
-	}
-	R := mul(&G, k0)
-	assertNotInfinity(R)
-
-	var k *big.Int
-	if hasEvenY(R) {
-		k = k0
-	} else {
-		k = new(big.Int).Sub(n, k0)
-	}
-
-	// hashTag2 = bytesFromPoint(R) + bytesFromPoint(P) + message
-	hashTag2 := append(bytesFromPoint(*R), bytesFromPoint(*P)...)
-	hashTag2 = append(hashTag2, message...)
-	tagged := taggedHash("BIP0340/challenge", hashTag2)
-	e := new(big.Int).Mod(intFromBytes(tagged), n)
-
-	sig := bytesFromPoint(*R)
-	sig = append(sig, bytesFromInt(new(big.Int).Mod(
-		new(big.Int).Add(k, new(big.Int).Mul(e, d)),
-		n,
-	))...)
-
-	sig2 := make([]byte, 64)
-	copy(sig2, sig) // that's mutability for ya
-
-	if !schnorrVerify(message, bytesFromPoint(*P), sig2) {
-		panic("The created signature does not pass verification.")
-	}
-	return sig
-}
-
 func schnorrVerify(message, pubkey, sig []byte) bool {
 	if len(pubkey) != 32 {
 		panic(fmt.Sprintf("The public key must be a 32-byte array, got %d.", len(pubkey)))
@@ -274,90 +186,21 @@ func schnorrVerify(message, pubkey, sig []byte) bool {
 	return R != nil && hasEvenY(R) && x(R).Cmp(r) == 0
 }
 
-// aux
-
-func bytesFromHex(str string) []byte {
-	b, _ := hex.DecodeString(str)
-	return b
-}
-
-func hexFromBytes(b []byte) string {
-	return hex.EncodeToString(b)
-}
-
-// it's Testing Time
-
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
 
-	file, err := os.Open("test-vectors.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	r := csv.NewReader(file)
-
-	allPassed := true
-
-	for {
-		row, err := r.Read()
-		if err != nil {
-			break
-		}
-		index := row[0]
-		seckeyHex := row[1]
-		pubkeyHex := row[2]
-		auxRandHex := row[3]
-		msgHex := row[4]
-		sigHex := row[5]
-		resultStr := row[6]
-		comment := row[7]
-
-		pubkey := bytesFromHex(pubkeyHex)
-		msg := bytesFromHex(msgHex)
-		sig := bytesFromHex(sigHex)
-		result := resultStr == "TRUE"
-
-		fmt.Printf("Test vector %s:\n", index)
-		if seckeyHex != "" {
-			seckey := bytesFromHex(seckeyHex)
-			pubkeyActual := pubkeyGen(seckey)
-			// compare pubkey with pubkeyActual
-			if !bytes.Equal(pubkey, pubkeyActual) {
-				fmt.Println(" * Failed key generation.")
-				fmt.Println("   Expected key:", hexFromBytes(pubkey))
-				fmt.Println("     Actual key:", hexFromBytes(pubkeyActual))
-				allPassed = false
-			}
-			auxRand := bytesFromHex(auxRandHex)
-			sigActual := schnorrSign(msg, seckey, auxRand)
-			if bytes.Equal(sig, sigActual) {
-				fmt.Println(" * Passed signing test.")
-			} else {
-				fmt.Println(" * Failed signing test.")
-				fmt.Println("   Expected signature:", hexFromBytes(sig))
-				fmt.Println("     Actual signature:", hexFromBytes(sigActual))
-				allPassed = false
-			}
-		}
-		resultActual := schnorrVerify(msg, pubkey, sig)
-		if resultActual == result {
-			fmt.Println(" * Passed verification test.")
-		} else {
-			fmt.Println(" * Failed verification test.")
-			fmt.Println("   Expected verification result:", result)
-			fmt.Println("     Actual verification result:", resultActual)
-			if comment != "" {
-				fmt.Println("   Comment:", comment)
-			}
-			allPassed = false
-		}
+	if len(args) < 4 {
+		fmt.Fprint(os.Stderr, "Too few arguments")
+		os.Exit(1)
 	}
 
-	if allPassed {
-		fmt.Println("\nAll tests passed.")
-	} else {
-		fmt.Println("\nSome tests failed.")
+	command := args[0]
+	
+	if command == "verify" {
+		hash, _ := hex.DecodeString(args[1])
+		pubkey, _ := hex.DecodeString(args[2])
+		sig, _ := hex.DecodeString(args[3])
+
+		fmt.Println(schnorrVerify(hash, pubkey, sig))
 	}
 }
